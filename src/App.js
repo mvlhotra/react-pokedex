d28741576735a1b7077d57739d0eabb84c54b467
Nik Malhotra <nmal@me.com>
2019-06-13 11:37:23 -0400

    exercise complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import Pokedex from './Pokedex'
+import Pokegame from './Pokegame'
 
 function App() {
   return (
     <div className="App">
-      <Pokedex />
+      <h1 id="title">Pokégame</h1>
+      <Pokegame />
     </div>
   );
 }
--- src/Pokecard.js
+++ src/Pokecard.js
@@ -5,18 +5,14 @@ function formatNumber(num) {
   return ('000' + num).substr(-3);
 }
 class Pokecard extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="card">
-        <h3 className="name">{this.props.pokeData.name}</h3>
-        <img src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${formatNumber(this.props.pokeData.id)}.png`} />
+        <h3 className="name">{this.props.name}</h3>
+        <img alt={`${this.props.name}`} src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${formatNumber(this.props.id)}.png`} />
         <div className="stats">
-          <p>Type: {this.props.pokeData.type + "\n"}</p>
-          <p>EXP: {this.props.pokeData.base_experience}</p>
+          <p>Type: {this.props.type}</p>
+          <p>EXP: {this.props.exp}</p>
         </div>
       </div>
     );
